2f6de9151d5593ae1df9e4e89ea346cb50152c42
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-04-10 10:38:44 +0700

    Add project files.

---
+++ Classlatihan1/Classlatihan1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+    string nim;
+    string nama;
+public: void cetak() {
+    cout << "NIM = " << nim;
+    cout << "\nNama = " << nama;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
