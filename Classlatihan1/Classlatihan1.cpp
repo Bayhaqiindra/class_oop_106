69e6acd24c671b656895873d12525b685c21e6fb
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-04-10 10:39:36 +0700

    membuat variable

--- Classlatihan1/Classlatihan1.cpp
+++ Classlatihan1/Classlatihan1.cpp
@@ -5,13 +5,3 @@ class Mahasiswa {
 public:
     string nim;
     string nama;
-public: void cetak() {
-    cout << "NIM = " << nim;
-    cout << "\nNama = " << nama;
-    }
-};
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
